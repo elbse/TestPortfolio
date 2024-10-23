9c77fe5568e5629e1e23dc64e3e6f0c07d3d9d7d
ketsuneene <c.priego.545409@umindanao.edu.ph>
2024-10-23 18:36:43 +0800

    Added new files

---
+++ inex.js
@@ -0,0 +1,19 @@
+let age = window.prompt("how old are you?");
+
+
+age = Number(age);
+age += 1;
+
+console.log("Happy birthday!! You are", age, "years");
+
+const PI = 3.14159;
+let radius;
+let circumference;
+
+radius = window.prompt("Enter the radius of a circle: ");
+radius = Number(radius);
+
+circumference = 2 * PI * radius;
+
+console.log("the circumference is: ", circumference);
+
